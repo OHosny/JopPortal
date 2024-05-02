d4c12b1f4696b79d51d3771b301408ecfb156357
AkramSwilam <akramswilam@outlook.com>
2024-05-02 06:21:36 +0300

    firebase initiated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDj826k_kedDmgXxW8qVopfBb2rC5gavPU",
+  authDomain: "job-portal-angular.firebaseapp.com",
+  projectId: "job-portal-angular",
+  storageBucket: "job-portal-angular.appspot.com",
+  messagingSenderId: "145187902094",
+  appId: "1:145187902094:web:2e7a91dba50804785a6fdf"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth())
+    )
+  ],
 };
